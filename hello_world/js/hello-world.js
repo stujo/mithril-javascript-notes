81c76a6db74a6f206538c716c156cb28420fc023
Stujo <stujo@users.noreply.github.com>
2016-12-13 21:50:45 -0800

    Add eslint

--- hello_world/js/hello-world.js
+++ hello_world/js/hello-world.js
@@ -1,3 +1,5 @@
+/*global m*/
+
 var helloWorldApp = {};
 
 //the Banner (model?) class has one property
@@ -13,43 +15,43 @@ function arrayLoop(messages){
   var index = 0;
 
   return function messageHelper(){
-     if(index >= messages.length){
-       index = 0;
-     }
-     return messages[index++];
-  }
+    if(index >= messages.length){
+      index = 0;
+    }
+    return messages[index++];
+  };
 }
 
 //define the view-model
 helloWorldApp.vm = {
 
-    init: function() {
-        helloWorldApp.vm.banner = new helloWorldApp.Banner(helloWorldApp.vm.messageGenerator());
+  init: function() {
+    helloWorldApp.vm.banner = new helloWorldApp.Banner(helloWorldApp.vm.messageGenerator());
 
-        // Set the message
-        helloWorldApp.vm.set = function() {
-            helloWorldApp.vm.banner.message(helloWorldApp.vm.messageGenerator());
-        };
-    },
-    messageGenerator : arrayLoop([
-      'Hello World!',
-      '42? Fourty-Two???',
-      'Goodbye!',
-      'Thanks for All the Fish'])
+    // Set the message
+    helloWorldApp.vm.set = function() {
+      helloWorldApp.vm.banner.message(helloWorldApp.vm.messageGenerator());
+    };
+  },
+  messageGenerator : arrayLoop([
+    'Hello World!',
+    '42? Fourty-Two???',
+    'Goodbye!',
+    'Thanks for All the Fish'])
 };
 
 //define the controller
 helloWorldApp.controller = function() {
-    helloWorldApp.vm.init();
+  helloWorldApp.vm.init();
 };
 
 helloWorldApp.view = function() {
-    return m("html", [
-        m("body", [
-           m("h1", helloWorldApp.vm.banner.message() || "N/A"),
-           m("button", {onclick: helloWorldApp.vm.set}, "Go!!"),
-        ])
-    ]);
+  return m('html', [
+    m('body', [
+      m('h1', helloWorldApp.vm.banner.message() || 'N/A'),
+      m('button', {onclick: helloWorldApp.vm.set}, 'Go!!'),
+    ])
+  ]);
 };
 
 
