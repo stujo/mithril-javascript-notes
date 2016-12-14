846cd4e3b71e71c06c247c6d27ea1d3c5fc9f2b4
Stujo <stujo@users.noreply.github.com>
2016-12-13 16:52:21 -0800

    Add array looping generator

--- hello_world/js/hello-world.js
+++ hello_world/js/hello-world.js
@@ -3,22 +3,39 @@ var helloWorldApp = {};
 //the Banner (model?) class has one property
 
 helloWorldApp.Banner = function(message) {
-  console.log("BANNER", message)
   // m.prop is a factory for a setter/getter function
   this.message = m.prop(message);
 };
 
+// NOT SPECIFICALLY FOR Mithril!
+// Funky helper function to give us a loop of messages
+function arrayLoop(messages){
+  var index = 0;
+
+  return function messageHelper(){
+     if(index >= messages.length){
+       index = 0;
+     }
+     return messages[index++];
+  }
+}
+
 //define the view-model
 helloWorldApp.vm = {
+
     init: function() {
-        helloWorldApp.vm.banner = new helloWorldApp.Banner();
+        helloWorldApp.vm.banner = new helloWorldApp.Banner(helloWorldApp.vm.messageGenerator());
 
         // Set the message
         helloWorldApp.vm.set = function() {
-            var message = 'Hello World!';
-            helloWorldApp.vm.banner.message(message);
+            helloWorldApp.vm.banner.message(helloWorldApp.vm.messageGenerator());
         };
-    }
+    },
+    messageGenerator : arrayLoop([
+      'Hello World!',
+      '42? Fourty-Two???',
+      'Goodbye!',
+      'Thanks for All the Fish'])
 };
 
 //define the controller
